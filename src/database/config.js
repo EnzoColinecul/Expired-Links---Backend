55a23233593f8d17bad73cf359a79a47f4b59ab5
Enzo Colinecul <enzocolinecul@MAC-66632.local>
2022-03-05 16:44:54 -0300

    test complete

--- src/database/config.js
+++ src/database/config.js
@@ -5,17 +5,16 @@ const client = new MongoClient(process.env.MONGODB_CNN);
 const DbConnection = async () => {
   try {
     await client.connect();
-    const database = client.db('links');
-    const linksDB = database.collection('data');
+    const database = client.db('links').collection('data');
+    await database.createIndex({ expireAt: 1 }, { expireAfterSeconds: 0 });
 
-    await linksDB.createIndex({ expireAt: 1 }, { expireAfterSeconds: 0 });
-
+    const time = new Date();
     const doc = {
-      information: 'hello word',
-      expireAt: new Date(10000),
+      information: 'hello word 2',
+      expireAt: new Date(time.setMinutes(time.getMinutes() + 5)),
     };
 
-    await linksDB.insertOne(doc);
+    await database.insertOne(doc);
     console.log('ta ok');
   } catch (err) {
     throw new Error(err);
